dd48d1d1692b7a4b625e7db79e4ef40798a93993
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-12-11 11:32:46 +0200

    LoadingHeight

--- src/app/category/[type]/(type-layout)/loading.tsx
+++ src/app/category/[type]/(type-layout)/loading.tsx
@@ -3,7 +3,7 @@ export default function CategoryLoading() {
         <div className="grid grid-cols-3 gap-3 tab:grid-cols-2 my-5">
             {
                 [...Array(9)].map((_, index) => (
-                    <div key={index} className="loader w-full h-[40rem] rounded-lg"></div>
+                    <div key={index} className="loader w-full h-[35rem] tab:h-[27rem] rounded-lg"></div>
                 ))
             }
         </div>
