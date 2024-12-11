7bbe6233c5103e914226ec92f0698dd2ad647692
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-12-11 19:33:30 +0200

    FixingRoutes

--- src/app/api/category/[type]/route.ts
+++ src/app/api/category/[type]/route.ts
@@ -1,26 +1,26 @@
-import { FetchToCache } from "@/hooks/ProductsHooks/FetchToCache";
-import { Product } from "@/interfaces/Types";
-import { NextRequest, NextResponse } from "next/server";
+// import { FetchToCache } from "@/hooks/ProductsHooks/FetchToCache";
+// import { Product } from "@/interfaces/Types";
+// import { NextRequest, NextResponse } from "next/server";
 
-let cachedDT: Map<string | undefined, Product[]> | null = null;
-let cachedProducts: Product[] | null = null;
+// let cachedDT: Map<string | undefined, Product[]> | null = null;
+// let cachedProducts: Product[] | null = null;
 
-const toSeeCached = () => {
-    const { groupTypes, parsedJson } = FetchToCache();
-    cachedDT = groupTypes;
-    cachedProducts = parsedJson?.products;
-};
+// const toSeeCached = () => {
+//     const { groupTypes, parsedJson } = FetchToCache();
+//     cachedDT = groupTypes;
+//     cachedProducts = parsedJson?.products;
+// };
 
-export async function GET(req: NextRequest) {
-    const type: string | undefined = req.nextUrl.pathname.split('/').pop();
-    try {
-        if (!cachedDT || !cachedProducts) {
-            toSeeCached();
-        }
-        const filteredProducts = cachedDT?.get(type);
-        if (type === 'all') return NextResponse.json(cachedProducts);
-        return NextResponse.json(filteredProducts)
-    } catch (error) {
-        throw new Error('Sorry, try again!')
-    }
-};
+// export async function GET(req: NextRequest) {
+//     const type: string | undefined = req.nextUrl.pathname.split('/').pop();
+//     try {
+//         if (!cachedDT || !cachedProducts) {
+//             toSeeCached();
+//         }
+//         const filteredProducts = cachedDT?.get(type);
+//         if (type === 'all') return NextResponse.json(cachedProducts);
+//         return NextResponse.json(filteredProducts)
+//     } catch (error) {
+//         throw new Error('Sorry, try again!')
+//     }
+// };
