bf277f3efec2b4e8a9732102d9b6265a8df9afd1
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-12-11 19:26:18 +0200

    DeletingForServer

--- src/app/category/[type]/(product-layout)/[title]/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-export default function ProductLayout({ children }: { children: ReactNode }) {
-    return (
-        <main className="py-16 my-10 px-3">
-            {children}
-        </main>
-    )
-}
--- src/app/category/[type]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-const NotFoundCategory = ({ type }: { type: string }) => {
-    return (
-        <div className="flex h-dvh w-full justify-center items-center text-2xl gap-2">
-            <p>404 | No page called</p>
-            <p className="text-red-500">{type}</p>
-        </div>
-    )
-}
-export default NotFoundCategory;
