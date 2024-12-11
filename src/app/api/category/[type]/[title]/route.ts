866e44f6de3589782e8aa17a6dd68ab9ed86bdc4
Abdullah ElMetwali <abdullahelmetwali@icloud.com>
2024-12-11 19:37:30 +0200

    FixingRoutes

--- src/app/api/category/[type]/[title]/route.ts
+++ src/app/api/category/[type]/[title]/route.ts
@@ -1,22 +1,22 @@
 // import { FetchToCache } from "@/hooks/ProductsHooks/FetchToCache";
 // import { Product } from "@/interfaces/Types";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // const { groupTypes, parsedJson } = FetchToCache();
 // let product: Product | undefined = undefined;
 
-// export async function GET(req: NextRequest) {
-//     const title = req.nextUrl.pathname.split('/').pop();
-//     const type = req.nextUrl.pathname.split('/')[3];
-//     // console.log(type)
-//     try {
-//         if (type === 'all') {
-//             product = parsedJson?.products?.find((pro: Product) => pro.title.replaceAll(' ', '-').toLowerCase() === title)
-//         } else {
-//             product = groupTypes?.get(type)?.find((pro: Product) => pro.title.replaceAll(' ', '-').toLowerCase() === title)
-//         }
-//         return NextResponse.json(product)
-//     } catch (error) {
-//         return NextResponse.json({ data: { message: "Can't get the product." } })
-//     }
-// }
+export async function GET(req: NextRequest) {
+    const title = req.nextUrl.pathname.split('/').pop();
+    const type = req.nextUrl.pathname.split('/')[3];
+    try {
+        // if (type === 'all') {
+        //     product = parsedJson?.products?.find((pro: Product) => pro.title.replaceAll(' ', '-').toLowerCase() === title)
+        // } else {
+        //     product = groupTypes?.get(type)?.find((pro: Product) => pro.title.replaceAll(' ', '-').toLowerCase() === title)
+        // }
+        // return NextResponse.json(product)
+        return NextResponse.json({ 'tst': 'tst' })
+    } catch (error) {
+        return NextResponse.json({ data: { message: "Can't get the product." } })
+    }
+}
